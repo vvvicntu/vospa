d29ca35b164e03981e81a81d25de205108e0c5b7
Воспанчук Виктория <raleevaa@gmail.com>
2025-05-01 13:59:54 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,83 @@
+const books = [
+    {
+        author: 'Этнони Берджес',
+        name: '1985',
+        publicationYear: 1978,
+        price: 500,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Кристина Старк',
+        name: 'Крылья',
+        publicationYear: 2015,
+        price: 400,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Джонатан Сафран Фоер',
+        name: 'Жутко громко и запредельно близко',
+        publicationYear: 2005,
+        price: 300,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Джоан Роулинг',
+        name: 'Краткое содержание «Гарри Поттер и философский камень',
+        publicationYear: 1997,
+        price: 550,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Антуан де Сент-Экзюпери',
+        name: 'Маленький принц',
+        publicationYear: 1943,
+        price: 600,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Габриэль Гарсиа Маркес',
+        name: 'Сто лет одиночества',
+        publicationYear: 1967,
+        price: 530,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Рэя Брэдбери',
+        name: '451 градус по Фаренгейту',
+        publicationYear: 1953,
+        price: 370,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Герман Мелвилла',
+        name: 'Моби Дик',
+        publicationYear: 1851,
+        price: 400,
+        bestseller: false,
+        inStock: true
+    },
+    {
+        author: 'Владимир Набоков',
+        name: 'Лолита',
+        publicationYear: 1955,
+        price: 200,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Олдос Леонард Хаксли',
+        name: 'О дивный новый мир',
+        publicationYear: 1931,
+        price: 520,
+        bestseller: false,
+        inStock: true
+    },
+
+];
